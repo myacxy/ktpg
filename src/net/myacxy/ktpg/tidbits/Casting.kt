40cd3d599b360cff43c086cab25944e892568e09
myacxy <myacxy@gmail.com>
2017-11-16 14:51:08 +0100

    [Basics] smart casting

---
+++ src/net/myacxy/ktpg/tidbits/Casting.kt
@@ -0,0 +1,43 @@
+package net.myacxy.ktpg.tidbits
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    var input: Any = 10
+
+    // -> ClassCastException
+    // no implicit conversions in Kotlin
+    try {
+        val str = input as String
+        println("str=${str.length}")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+
+    val str2 = input as? String
+    println("str2=${str2?.length}")
+}
+
+open class Person {
+
+}
+
+class Employee: Person() {
+
+    fun vacationDays(days: Int) {
+
+    }
+}
+
+class Contractor: Person() {
+
+}
+
+fun hasVacation(obj: Person) {
+    if (obj is Employee) {
+        // smart casting
+        obj.vacationDays(20)
+    }
+}
